54b88d8ecfcb9b8e2c3e276d06d84a6d23e86870
nikolaiKorenev99 <nikolaj.korenev99@gmail.com>
2021-01-02 19:03:10 +0200

    Lab 7  Switch Statements fixed - Customer.withdraw method has a some switches - Does not respect OCP (Open Closed Principle) - Every time we want to add a new customer type we have to modify the two switches

--- src/main/java/com/github/nikolaiKorenev99/software_reengineering_and_verification/Lab7/Customer.java
+++ src/main/java/com/github/nikolaiKorenev99/software_reengineering_and_verification/Lab7/Customer.java
@@ -1,22 +1,23 @@
 package com.github.nikolaiKorenev99.software_reengineering_and_verification.Lab7;
 
 public class Customer extends AbstractCustomer {
-    private CustomerType customerType;
+    private DiscountForCustomer discountForCustomer;
     private Account account;
-    private double companyOverdraftDiscount = 1;
+    public static final double COMPANY_DISCOUNT = 1;
+    public static final double PREMIUM_COMPANY_DISCOUNT = 2;
 
     public Customer(String name, String surname, String email, CustomerType customerType, Account account) {
         super(name, surname, email);
-        this.customerType = customerType;
         this.account = account;
+        discountForCustomer = createDiscountCustomer(customerType, COMPANY_DISCOUNT, COMPANY_DISCOUNT);
     }
 
     // use only to create companies
     public Customer(String name, String email, Account account, double companyOverdraftDiscount) {
         super(name, email);
-        this.customerType = CustomerType.COMPANY;
         this.account = account;
-        this.companyOverdraftDiscount = companyOverdraftDiscount;
+        discountForCustomer = createDiscountCustomer(CustomerType.COMPANY, companyOverdraftDiscount,
+                PREMIUM_COMPANY_DISCOUNT);
     }
 
     public void withdraw(double sum, String currency) {
@@ -26,24 +27,9 @@ public class Customer extends AbstractCustomer {
         withdrawCustomerTypeCheck(sum);
     }
 
-    public CustomerType getCustomerType() {
-        return customerType;
-    }
-
-    public void setCustomerType(CustomerType customerType) {
-        this.customerType = customerType;
-    }
-
-    public double getCompanyOverdraftDiscount() {
-        return companyOverdraftDiscount;
-    }
-
-    public void setCompanyOverdraftDiscount(double companyOverdraftDiscount) {
-        this.companyOverdraftDiscount = companyOverdraftDiscount;
-    }
-
     public String printCustomerDaysOverdrawn() {
-        String accountDescription = "Account: IBAN: " + account.getIban() + ", Days Overdrawn: " + account.getDaysOverdrawn();
+        String accountDescription = "Account: IBAN: " + account.getIban()
+                + ", Days Overdrawn: " + account.getDaysOverdrawn();
         return getFullName() + accountDescription;
     }
 
@@ -58,15 +44,7 @@ public class Customer extends AbstractCustomer {
                 + account.getMoney() + ", Account type: " + account.getType();
     }
 
-    private void withdrawForPerson(double sum) {
-        if (account.getMoney() < 0) {
-            account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee());
-        } else {
-            account.setMoney(account.getMoney() - sum);
-        }
-    }
-
-    private void withdrawForCompany(double sum, double companyOverdraftDiscount) {
+    private void withdrawOverDraft(double sum, double companyOverdraftDiscount) {
         if (account.getMoney() < 0) {
             // no discount for overdraft for not premium account
             account.setMoney((account.getMoney() - sum) - sum * account.overdraftFee() * companyOverdraftDiscount);
@@ -76,19 +54,36 @@ public class Customer extends AbstractCustomer {
     }
 
     private void withdrawCustomerTypeCheck(double sum) {
-        switch (customerType) {
-            case COMPANY:
-                if (account.getType().isPremium()) {
-                    // 50 percent discount for with premium account
-                    withdrawForCompany(sum, companyOverdraftDiscount / 2);
-                } else {
-                    withdrawForCompany(sum, this.companyOverdraftDiscount);
-                }
-                break;
-            case PERSON:
-                withdrawForPerson(sum);
-                break;
+        if (account.getType().isPremium() && discountForCustomer.isCompany()) {
+            // 50 percent discount for with premium account
+            withdrawOverDraft(sum, (discountForCustomer.companyOverdraftDiscount
+                    / discountForCustomer.premiumDiscount));
+        } else {
+            withdrawOverDraft(sum, discountForCustomer.companyOverdraftDiscount);
         }
     }
+
+    private class DiscountForCustomer {
+        private CustomerType customerType;
+        private double companyOverdraftDiscount;
+        private double premiumDiscount;
+
+        public DiscountForCustomer(CustomerType customerType, double companyOverdraftDiscount,
+                                   double premiumDiscount) {
+            this.customerType = customerType;
+            this.companyOverdraftDiscount = companyOverdraftDiscount;
+            this.premiumDiscount = premiumDiscount;
+        }
+
+        public boolean isCompany() {
+            return customerType == CustomerType.COMPANY;
+        }
+    }
+
+    private DiscountForCustomer createDiscountCustomer(CustomerType customerType,
+                                                       double companyOverdraftDiscount, double premiumDiscount) {
+        return new DiscountForCustomer(customerType, companyOverdraftDiscount, premiumDiscount);
+    }
+
 }
 
